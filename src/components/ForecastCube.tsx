c22027ad5e6a72b93f8d736804ae3feeb5694418
eryk <e.szczepaniec@lightpixel.io>
2025-11-27 11:12:26 +0100

    fix(ui): add boilerplate data

--- src/components/ForecastCube.tsx
+++ src/components/ForecastCube.tsx
@@ -4,22 +4,22 @@ const ForecastCube: React.FC = () => {
     <div className="max-w-2xl mx-auto mt-6 flex flex-wrap justify-center gap-4">
       <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
         <span className="mb-4 block">Feels like</span>
-        <span className="block text-2xl">64C</span>
+        <span className="block text-3xl">64C</span>
       </div>
 
       <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
-        <span className="mb-4 block">Feels like</span>
-        <span className="block text-2xl">64C</span>
+        <span className="mb-4 block">Humidity</span>
+        <span className="block text-3xl">46%</span>
       </div>
 
       <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
-        <span className="mb-4 block">Feels like</span>
-        <span className="block text-2xl">64C</span>
+        <span className="mb-4 block">Wind</span>
+        <span className="block text-3xl">9mph</span>
       </div>
 
       <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
-        <span className="mb-4 block">Feels like</span>
-        <span className="block text-2xl">64C</span>
+        <span className="mb-4 block">Precipitation</span>
+        <span className="block text-3xl">0 in</span>
       </div>
     </div>
   );
