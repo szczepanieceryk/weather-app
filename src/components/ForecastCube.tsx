ec42a869a6a6664be526bbca7e5b6e7fee5595b2
eryk <e.szczepaniec@lightpixel.io>
2025-12-01 13:27:34 +0100

    fix(ui): fix desktop dsplay

--- src/components/ForecastCube.tsx
+++ src/components/ForecastCube.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 const ForecastCube: React.FC = () => {
   return (
-    <div className="max-w-[95%] mx-auto mt-6 flex flex-wrap justify-center gap-4">
-      <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
+    <div className="max-w-[95%] lg:max-w-[60%] mx-auto mt-6 flex flex-wrap justify-center lg:justify-afound gap-4 ">
+      <div className="p-4 min-w-[150px] max-w-200px] rounded-lg bg-[#3d3b5eff] text-white">
         <span className="mb-4 block">Feels like</span>
         <span className="block text-3xl">64C</span>
       </div>
 
-      <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
+      <div className="p-4 min-w-[150px] max-w-200px] rounded-lg bg-[#3d3b5eff] text-white">
         <span className="mb-4 block">Humidity</span>
         <span className="block text-3xl">46%</span>
       </div>
 
-      <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
+      <div className="p-4 min-w-[150px] max-w-200px] rounded-lg bg-[#3d3b5eff] text-white">
         <span className="mb-4 block">Wind</span>
         <span className="block text-3xl">9mph</span>
       </div>
 
-      <div className="p-4 min-w-[150px] rounded-lg bg-[#3d3b5eff] text-white">
+      <div className="p-4 min-w-[150px] max-w-200px] rounded-lg bg-[#3d3b5eff] text-white">
         <span className="mb-4 block">Precipitation</span>
         <span className="block text-3xl">0 in</span>
       </div>
--- src/components/TodayForecast.tsx
+++ src/components/TodayForecast.tsx
@@ -17,20 +17,20 @@ const TodayForecast: React.FC = () => {
   return (
     <div>
       <div
-        className="max-w-[95%] md:max-w-2xl mx-auto mt-[3rem] py-[3rem] px-2 flex flex-wrap justify-center text-center rounded-xl bg-[#4455daff]"
+        className="max-w-[95%] lg:max-w-[60%] mx-auto mt-[3rem] py-[3rem] lg:py-[4rem] px-2 flex flex-wrap justify-center lg:flex-start text-center lg:text-left lg:items-center rounded-xl bg-[#4455daff]"
         style={{
           backgroundImage: `url(${returnAppropriateBgImg()})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
       >
-        <div className="basis-full mb-4 ">
+        <div className="basis-full lg:max-w-[300px] mb-4 lg:mb-0">
           <span className="block mb-2 text-white text-2xl">
             <strong>Berlin, Germany</strong>
           </span>
           <span className="block text-white">Tuesday, Aug 5,2025</span>
         </div>
-        <div className="mt-6 basis-full flex flex-wrap items-center justify-around">
+        <div className="mt-6 lg:mt-0 basis-full lg:max-w-[300px] flex flex-wrap items-center justify-around">
           <img src={sunIcon} alt="" className="w-[120px] h-[120px] " />
           <span className="text-7xl text-white">68C</span>
         </div>
