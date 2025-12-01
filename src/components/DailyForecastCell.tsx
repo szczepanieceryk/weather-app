12d3924515afe2112739446e10a26cf6a323108e
eryk <e.szczepaniec@lightpixel.io>
2025-12-01 13:30:27 +0100

    fix(ui): adjuct section desktop display

--- src/components/DailyForecastCell.tsx
+++ src/components/DailyForecastCell.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { DailyForecastCellProps } from '../types/types';
 const DailyForecastCell: React.FC<DailyForecastCellProps> = ({ day, weatherIcon, temperature }) => {
   return (
-    <div className="p-1 py-3 max-w-[100px] flex flex-wrap justify-center text-center mx-1 rounded-lg text-white bg-[#3d3b5eff]">
+    <div className="p-1 py-3 max-w-[100px] lg:max-w-[85px] flex flex-wrap justify-center text-center mx-1 rounded-xl text-white bg-[#3d3b5eff]">
       <span className="mb-2 block basis-full">{day}</span>
       <img src={weatherIcon} alt="" className="my-2 w10 h-10" />
       <div className="basis-full flex justify-between max-w-[70px] mx-auto">
