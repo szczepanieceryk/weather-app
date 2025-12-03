e29620288bc8ba7d98183883d822b0e93b1c3a51
eryk <e.szczepaniec@lightpixel.io>
2025-12-03 14:26:43 +0100

    fix(ui): style search input display on desktop

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -7,19 +7,21 @@ const Header: React.FC = () => {
         How&apos;s the sky looking today?
       </h1>
       <form action="" className="w-full mx-auto md:max-w-lg">
-        <div className="relative">
-          <img src={searchIcon} alt="" className="absolute top-3 left-3" />
-          <label htmlFor="">
-            <input
-              type="search"
-              placeholder="Search for a place..."
-              className="p-2 pl-[3rem] rounded-md bg-[#3d3b5eff] w-full"
-            />
-          </label>
+        <div className="flex flex-wrap lg:flex-nowrap items-center lg:gap-2">
+          <div className="relative w-full lg:w-3/4">
+            <img src={searchIcon} alt="" className="absolute top-3 left-3" />
+            <label htmlFor="">
+              <input
+                type="search"
+                placeholder="Search for a place..."
+                className="p-2 pl-[3rem] rounded-md bg-[#3d3b5eff] w-full"
+              />
+            </label>
+          </div>
+          <button className="mt-2 lg:mt-0 p-2 block w-full lg:w-1/5 rounded-md bg-[#4455daff] text-white">
+            Search
+          </button>
         </div>
-        <button className="mt-2 p-2 block w-full rounded-md bg-[#4455daff] text-white">
-          Search
-        </button>
       </form>
     </div>
   );
