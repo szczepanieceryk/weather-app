038741cc1b192e5ebe960e74b9f037cedcdbac40
eryk <e.szczepaniec@lightpixel.io>
2026-01-05 13:35:37 +0100

    feat(ui): add 'Retry' button to Error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import errorIcon from '../assets/images/icon-error.svg';
+import retryIcon from '../assets/images/icon-retry.svg';
+
 const ErrorPage: React.FC = () => {
   return (
     <div className="flex flex-wrap justify-center text-center max-w-lg mx-auto">
@@ -10,6 +12,11 @@ const ErrorPage: React.FC = () => {
       <p className="text-[#aeaeb7ff] max-w-md">
         We couldn`t connect to the server (API error). Please try again in a few moments
       </p>
+
+      <button className="mt-5 p-3 rounded-md bg-[#272541ff] text-white">
+        <img className="inline-block mr-2" src={retryIcon} alt="" />
+        <span className="inline-block">Retry</span>
+      </button>
     </div>
   );
 };
