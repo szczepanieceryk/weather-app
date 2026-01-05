ab9426dcfe90288620c7f5ffb1f03af459cc775e
eryk <e.szczepaniec@lightpixel.io>
2026-01-05 13:48:01 +0100

    feat(ui): add dropdown icon & hide default seelct display

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
 import unitsIcon from '../assets/images/icon-units.svg';
+import dropdownIcon from '../assets/images/icon-dropdown.svg';
+
 const Navigation: React.FC = () => {
   return (
     <nav>
@@ -8,9 +10,9 @@ const Navigation: React.FC = () => {
         <img src={logo} alt="" />
         <div className="p-2 rounded-md bg-[#3d3b5eff] cursor-pointer">
           <img src={unitsIcon} alt="" className="inline" />
-          <label htmlFor="units-select-id" className="inline ml-2">
+          <label htmlFor="units-select-id" className="inline mx-2 text-white inline-block">
             Units
-            <select name="" id="units-select-id" className="">
+            <select name="" id="units-select-id" className="hidden">
               <option value=""></option>
               <option value="">Switch to Imperial</option>
               <optgroup label="Temperature">
@@ -29,6 +31,7 @@ const Navigation: React.FC = () => {
               </optgroup>
             </select>
           </label>
+          <img src={dropdownIcon} alt="" className="inline-block" />
         </div>
       </div>
     </nav>
