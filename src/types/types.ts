d01ba774b95e95aedf68632fbcf502edb0969f3d
eryk <e.szczepaniec@lightpixel.io>
2026-01-08 16:29:14 +0100

    fix: update WeatherData interface to match API response

--- src/types/types.ts
+++ src/types/types.ts
@@ -26,28 +26,32 @@ export interface WeatherData {
   timezone: string;
   timezone_abbreviation: string;
   elevation: number;
-  current: {
+
+  current_weather: {
     time: string;
-    interval: number;
-    temperature_2m: number;
-    wind_speed_10m: number;
+    temperature: number;
+    windspeed: number;
+    winddirection?: number;
+    weathercode?: number;
   };
-  current_units: {
-    time: string;
-    interval: string;
-    temperature_2m: string;
-    wind_speed_10m: string;
+
+  current_weather_units?: {
+    temperature?: string;
+    windspeed?: string;
+    winddirection?: string;
   };
+
   hourly: {
     time: string[];
     temperature_2m: number[];
-    relative_humidity_2m: number[];
-    wind_speed_10m: number[];
+    relativehumidity_2m: number[];
+    windspeed_10m: number[];
   };
+
   hourly_units: {
     time: string;
     temperature_2m: string;
-    relative_humidity_2m: string;
-    wind_speed_10m: string;
+    relativehumidity_2m: string;
+    windspeed_10m: string;
   };
 }
